782d46de01f0b6ca2be9333a47609c10a393d2b5
Pranav <prnvbirajdar@gmail.com>
2021-11-26 16:52:52 +0530

    exercise 1

--- lessons/01-rendering/exercise/SignupForm.js
+++ lessons/01-rendering/exercise/SignupForm.js
@@ -7,6 +7,33 @@ import { DateFields, MonthField, DayField, YearField } from "app/DateFields"
 // import SignupForm from "./SignupForm.final"
 // export default SignupForm
 
+
+const InputElement = ({ id, label, type = "text" }) => {
+  return (
+    <label htmlFor={id}>
+      <input type={type} label={label} placeholder={label} />
+    </label>
+  )
+}
+
 export default function SignupForm() {
-  return <form className="SignupForm">{/* Your work here */}</form>
+  return (
+    <form className="SignupForm">
+      <InputElement id="displayName" label="Display Name" />
+      <InputElement id="photoUrl" label="Photo Url" />
+      <InputElement id="email" label="Email" type="email" />
+      <InputElement id="password" label="Password" type="password" />
+
+      {/* <DateFields>
+          <MonthField/>
+          <DayField/>
+          <YearField start="1950" end="2021"/>
+        </DateFields> */}
+
+
+      <TabsButton>          
+        <FaDumbbell />
+      </TabsButton>
+    </form>
+  )
 }
