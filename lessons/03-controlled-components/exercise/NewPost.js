918856f1ece3279f74e46d0e371877c44ff95776
Pranav <prnvbirajdar@gmail.com>
2021-11-28 15:17:09 +0530

    exercise 3

--- lessons/03-controlled-components/exercise/NewPost.js
+++ lessons/03-controlled-components/exercise/NewPost.js
@@ -7,23 +7,29 @@ import RecentPostsDropdown from "app/RecentPostsDropdown"
 
 const errorClass = "NewPost_error"
 
-const MAX_MESSAGE_LENGTH = 200
+const MAX_MESSAGE_LENGTH = 10
 
 export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
   const [{ auth }] = useAppState()
 
+  const [message, setMessage] = useState('')
+
+  const handleChange = e => setMessage(e.target.value)
+
   return (
     <div className="NewPost">
       {showAvatar && <Avatar uid={auth.uid} size={70} />}
-      <form className="NewPost_form">
+      <form className={`NewPost_form ${message.length > MAX_MESSAGE_LENGTH ? errorClass : ''}`}>
         <textarea
           className="NewPost_input"
           placeholder="Tell us about your workout!"
+          value={message}
+          onChange={handleChange}
         />
         <div className="NewPost_char_count">
-          0/{MAX_MESSAGE_LENGTH}
+          {message.length}/{MAX_MESSAGE_LENGTH}
         </div>
-        <RecentPostsDropdown uid={auth.uid} onSelect={(message) => {}}/>
+        <RecentPostsDropdown uid={auth.uid} onSelect={(message) => {setMessage(message)}}/>
         <div className="NewPost_buttons">
           <Minutes date={date} />
           <div>
