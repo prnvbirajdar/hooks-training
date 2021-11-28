879d1612758ff73411974145ba66dc0fa4e49f89
Pranav <prnvbirajdar@gmail.com>
2021-11-28 22:53:46 +0530

    exercise 4

--- lessons/04-effects/exercise/NewPost.js
+++ lessons/04-effects/exercise/NewPost.js
@@ -11,22 +11,34 @@ const MAX_MESSAGE_LENGTH = 200
 
 export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
   const [{ auth }] = useAppState()
-  const [message, setMessage] = useState('Ran around the lake.')
+  const dateKey = makeNewPostKey(date)
+  const [message, setMessage] = useState(getLocalStorageValue(dateKey) || '')
   const messageTooLong = message.length > MAX_MESSAGE_LENGTH
 
-  function handleMessageChange(event) {
-    setMessage(event.target.value)
-  }
+  // useEffect and localStorage exercise
+  useEffect(() => {
+    setLocalStorage(dateKey, message)
+  }, [dateKey, message])
+
+  // useRef exercise
+  const messageRef = useRef()
+
+  useEffect(() => {
+    if(takeFocus)
+    messageRef.current.focus()
+  }, [takeFocus])
 
   return (
     <div className={'NewPost' + (messageTooLong ? ' NewPost_error' : '')}>
       {showAvatar && <Avatar uid={auth.uid} size={70} />}
       <form className="NewPost_form">
         <textarea
+          ref={messageRef}
           className="NewPost_input"
           placeholder="Tell us about your workout!"
           value={message}
-          onChange={handleMessageChange}
+          onChange={e => setMessage(e.target.value)
+}
         />
         <div className="NewPost_char_count">
           {message.length}/{MAX_MESSAGE_LENGTH}
