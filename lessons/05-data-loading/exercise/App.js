b5e9b7df8ed972d3f4e4d6313ed83b3c2dcbfefc
Pranav <prnvbirajdar@gmail.com>
2021-11-29 17:07:34 +0530

    exercise 5

--- lessons/05-data-loading/exercise/App.js
+++ lessons/05-data-loading/exercise/App.js
@@ -4,9 +4,25 @@ import { onAuthStateChanged } from 'app/utils'
 import LoggedIn from 'app/LoggedIn'
 import LoggedOut from 'app/LoggedOut'
 
+function useAuth() {
+  const [auth, setAuth] = useState(null)
+  const [authAttempted, setAuthAttempted] = useState(false)
+
+  useEffect(() => {
+     const unsubscribe = onAuthStateChanged(auth => {
+        setAuthAttempted(true)
+        setAuth(auth)
+    })
+
+    return unsubscribe
+
+  }, [])
+
+  return [auth, authAttempted]
+}
+
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  const [auth, authAttempted] = useAuth()
 
   if (!authAttempted) {
     return <p>Authenticating...</p>
