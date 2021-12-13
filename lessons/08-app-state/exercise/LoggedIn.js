4bfd3a41ec6b649843c0a0fa988365ec8db142fd
Pranav <prnvbirajdar@gmail.com>
2021-12-14 04:14:54 +0530

    exercise 8

--- lessons/08-app-state/exercise/LoggedIn.js
+++ lessons/08-app-state/exercise/LoggedIn.js
@@ -10,7 +10,18 @@ import User from "app/User"
 import NotFound from "app/NotFound"
 
 export default function LoggedIn() {
-  const user = null
+  const [{ auth, user }, dispatch] = useAppState()
+
+  useEffect(() => {
+
+    if (!user) {
+      fetchUser(auth.uid).then(user => {
+        dispatch({ type: 'LOAD_USER', user })
+      })
+    }
+
+  }, [auth.uid, dispatch, user])
+
 
   return user ? (
     <Fragment>
